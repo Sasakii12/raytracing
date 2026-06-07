bc652f38e7597c9d7dfadd1ddee6340402aecc6c
Vipershroom <undertalefan812@gmail.com>
2026-06-07 15:00:27 -0400

    Complete 2.3 - 3.1

--- main.cpp
+++ main.cpp
@@ -1,25 +1,22 @@
+#include "color.h"
+#include "vec3.h"
 #include <iostream>
 
 using namespace std;
 
 int main() {
-	int image_width = 256;
+	int image_width = 256 * 2;
 	int image_height = 256;
 
 	cout << "P3\n" << image_width << " " << image_height << "\n255\n";
 
 	for (int j = 0; j < image_height; j++) {
+		clog << "\r Scanlines remaining: " << (image_height - j) << ' ' << flush;
 		for (int i = 0; i < image_width; i++) {
-			auto r = double(i) / (image_width-1);
-			auto g = double(j) / (image_height-1);
-			auto b = 0.0;
-
-			int ir = int(255.999 * r);
-			int ig = int(255.999 * g);
-			int ib = int(255.999 * b);
-
-			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+			auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+			write_color(cout, pixel_color);
 		}
 	}
+	clog << "\r Done.              \n";
 }
 
